9d2029e89a7af121178c914e6115aeb5c968864f
nkamalu620 <nkamalu96@gmail.com>
2025-01-17 12:45:10 -0500

    more code in AboutMe.jsx file

--- new hire/src/pages/AboutMe.jsx
+++ new hire/src/pages/AboutMe.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 const AboutMe = () => {
   return (
-    <section>
+    <section style={aboutMeStyle}>
       <h2>About Me</h2>
-      <p>This is a brief bio about myself.</p>
+      <img src="/" alt="/" style ={imageStlye}/>
+      <p>Hi, my name is Nkechi. I am an asipring web developer who likes to be challenged on new things!</p>
     </section>
   );
 }
+
+const aboutMeStyle = {
+  textAlign: 'center',
+  padding: '2rem',
+};
+
+const imageStyle = {
+  borderRadius: '50%',
+  width: '150px',
+  height: '150px',
+};
 
 export default AboutMe;
